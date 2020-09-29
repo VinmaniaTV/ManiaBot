e421cfbd494413026e034ac6e275a6f362fac90e
Vinmania <vinmaniamc@gmail.com>
2020-09-29 02:44:12 +0200

    template

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  client.user.setActivity('!help', { type: 'PLAYING' });
+})
+
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! Ce message a une latence de ${timeTaken}ms.`);
+  }
+
+  else if (command === "sum") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`The sum of all the arguments you provided is ${sum}!`);
+  }
+});
+
+client.login(config.BOT_TOKEN);
